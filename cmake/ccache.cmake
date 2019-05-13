c1c1757cb5e480437afbf1cb2e130dc554fd05b1
wopeizl <wopeizl@icloud.com>
2019-05-13 10:40:16 +0800

    t

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -1,9 +1,19 @@
 # Use ccache if found ccache program
 
-find_program(CCACHE_PATH ccache)
+#find_program(CCACHE_PATH ccache)
+#
+#if(CCACHE_PATH)
+#    message(STATUS "Ccache is founded, use ccache to speed up compile.")
+#    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_PATH})
+#    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_PATH})
+#endif(CCACHE_PATH)
 
-if(CCACHE_PATH)
-    message(STATUS "Ccache is founded, use ccache to speed up compile.")
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_PATH})
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_PATH})
-endif(CCACHE_PATH)
+find_program(SCCACHE_PATH sccache)
+
+if(SCCACHE_PATH)
+    message(STATUS "sccache is founded, use ccache to speed up compile. ${SCCACHE_PATH}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${SCCACHE_PATH})
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${SCCACHE_PATH})
+else()
+    message(FATAL "no found sccache")
+endif(SCCACHE_PATH)
